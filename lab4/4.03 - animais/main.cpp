83e4a54e7cdedefc1c879e67f99e8f987d315f08
gabreuvcr <gabreuvcr@gmail.com>
2019-12-17 17:23:53 -0300

    nothing

--- lab4/4.03 - animais/main.cpp
+++ lab4/4.03 - animais/main.cpp
@@ -9,7 +9,7 @@ int main(void) {
     fazenda.push_back(new Cachorro("branco"));
     fazenda[0]->faz_barulho();
     popula(fazenda,fazenda[0], 10);
-    for(unsigned int i=0; i < fazenda.size(); i++) {
+    for(unsigned int i = 0; i < fazenda.size(); i++) {
         std::cout<<fazenda[i]->get_id()<<std::endl;
     }
 
